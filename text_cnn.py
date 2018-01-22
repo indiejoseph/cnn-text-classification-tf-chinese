933d6a7a05295d74ae15db22f8acbf516715eecc
zhzhx2008 <zhzhx2008@126.com>
2018-01-23 00:22:14 +0800

    Update to support tensorflow 1.2.1

--- text_cnn.py
+++ text_cnn.py
@@ -10,15 +10,44 @@ def highway(input_, size, layer_size=1, bias=-2, f=tf.nn.relu):
   """
   output = input_
   for idx in xrange(layer_size):
-    output = f(tf.nn.rnn_cell._linear(output, size, 0, scope='output_lin_%d' % idx))
+    output = f(linear(output, size, scope='output_lin_%d' % idx))
 
     transform_gate = tf.sigmoid(
-      tf.nn.rnn_cell._linear(input_, size, 0, scope='transform_lin_%d' % idx) + bias)
+      linear(input_, size, scope='transform_lin_%d' % idx) + bias)
     carry_gate = 1. - transform_gate
 
     output = transform_gate * output + carry_gate * input_
 
   return output
+
+
+def linear(input_, output_size, scope=None):
+  '''''
+  Linear map: output[k] = sum_i(Matrix[k, i] * args[i] ) + Bias[k]
+  Args:
+      args: a tensor or a list of 2D, batch x n, Tensors.
+  output_size: int, second dimension of W[i].
+  scope: VariableScope for the created subgraph; defaults to "Linear".
+  Returns:
+  A 2D Tensor with shape [batch x output_size] equal to
+  sum_i(args[i] * W[i]), where W[i]s are newly created matrices.
+  Raises:
+  ValueError: if some of the arguments has unspecified or wrong shape.
+  '''
+
+  shape = input_.get_shape().as_list()
+  if len(shape) != 2:
+    raise ValueError("Linear is expecting 2D arguments: %s" % str(shape))
+  if not shape[1]:
+    raise ValueError("Linear expects shape[1] of arguments: %s" % str(shape))
+  input_size = shape[1]
+
+  # Now the computation.
+  with tf.variable_scope(scope or "SimpleLinear"):
+    matrix = tf.get_variable("Matrix", [output_size, input_size], dtype=input_.dtype)
+    bias_term = tf.get_variable("Bias", [output_size], dtype=input_.dtype)
+
+  return tf.matmul(input_, tf.transpose(matrix)) + bias_term
 
 
 class TextCNN(object):
@@ -72,7 +101,7 @@ class TextCNN(object):
 
       # Combine all the pooled features
       num_filters_total = sum(num_filters)
-      self.h_pool = tf.concat(3, pooled_outputs)
+      self.h_pool = tf.concat(pooled_outputs, 3)
       self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
 
       # Add highway
@@ -94,7 +123,7 @@ class TextCNN(object):
 
       # CalculateMean cross-entropy loss
       with tf.name_scope("loss"):
-        losses = tf.nn.softmax_cross_entropy_with_logits(self.scores, self.input_y)
+        losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
         self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
 
       # Accuracy
